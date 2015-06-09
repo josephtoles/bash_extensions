76ec595107b06ceeca09ad6f42a5ef9423077a61
Joseph Toles <jbtoles@gmail.com>
2015-06-09 12:25:02 -0700

    Simple function works

--- recycle.sh
+++ recycle.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
-# Constants
-DEFAULT_RECYCLE_PATH=~/.recycle
-
-# Set RECYCLE_PATH
-echo "DEFAULT_RECYCLE_PATH is $DEFAULT_RECYCLE_PATH"
-if [ ! $RECYCLE_PATH ]
+# Set RECYCLE_DIRECTORY
+DEFAULT_RECYCLE_DIRECTORY="~/.recycle"
+if [ ! $RECYCLE_DIRECTORY ]
 then
-  RECYCLE_PATH=DEFAULT_RECYCLE_PATH
-  echo "setting RECYCLE_PATH to $RECYCLE_PATH"
+  RECYCLE_DIRECTORY=$DEFAULT_RECYCLE_DIRECTORY
 fi
 
-# check if file $1 exists
+# create recycling folder if necessary
+if [ ! -d $RECYCLE_DIRECTORY ]
+then
+  mkdir $RECYCLE_DIRECTORY
+fi
+
+# check that file $1 exists
 if [ ! -e $1 ]
 then
   echo "file $1 does not exist"
@@ -19,8 +21,15 @@ then
 fi
 
 # check if file exists in recycle directory. Append integer if necessary
-# TODO
+filename=`basename $1`
+target="$RECYCLE_DIRECTORY/$filename"
+index=0
+while [ -e $target ]
+do
+  let "index += 1"
+  target="${RECYCLE_DIRECTORY}/${filename}_${index}"
+done
 
 # move file to recycle directory
-# TODO
+mv $1 $target
 
